c64542482b9a8cccc4e1bd1679e26586f60c1167
bui huy bach <bachkame@gmail.com>
2022-09-21 01:14:48 +0700

    them image capture

--- auto1/Form1.cs
+++ auto1/Form1.cs
@@ -21,10 +21,10 @@ namespace auto1
             InitializeComponent();
         }
 
-        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
-        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
-        [DllImport("User32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+        //[DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        //public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        //[DllImport("User32.dll")]
+        //public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
 
         void click_to(Point point, int delay)
         {
@@ -52,11 +52,11 @@ namespace auto1
             var _startPoint = AutoControl.GetGlobalPoint(HWND, 50, 80);
             var _loginNowPoint = AutoControl.GetGlobalPoint(HWND, 180, 210);
 
-            click_to(_threeDotPoint, 500);
-            click_to(_displayAsyncPoint, 500);
-            click_to(_selectAllPoint, 500);
-            click_to(_startPoint, 500);
-            click_to(_loginNowPoint, 500);
+            click_to(_threeDotPoint, 800);
+            click_to(_displayAsyncPoint, 800);
+            click_to(_selectAllPoint, 800);
+            click_to(_startPoint, 800);
+            click_to(_loginNowPoint, 800);
         }
 
         void play_record(IntPtr HWND, int timePerGame)
@@ -67,10 +67,10 @@ namespace auto1
             var exitRecordPoint = AutoControl.GetGlobalPoint(HWND, 515, 85);
             var _stopPoint = AutoControl.GetGlobalPoint(HWND, 330, 20);
 
-            click_to(_threeDotPoint, 1000);
-            click_to(displayRecordPoint, 1000);
-            click_to(playRecordPoint,1000);
-            click_to(exitRecordPoint, 1000);
+            click_to(_threeDotPoint, 800);
+            click_to(displayRecordPoint, 800);
+            click_to(playRecordPoint, 800);
+            click_to(exitRecordPoint, 800);
 
             Thread.Sleep(timePerGame);
             click_to(_stopPoint, 700);
@@ -85,6 +85,13 @@ namespace auto1
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\20010844\Desktop\data1.txt");
+            if(lines.Length == 0)
+            {
+                MessageBox.Show("File khong co j ca");
+                return;
+            }
+
             var hWnd = AutoControl.FindWindowHandle(null, "LDPlayer");
 
             //var openGamePoint = AutoControl.GetGlobalPoint(hWnd,225, 100);
@@ -95,38 +102,45 @@ namespace auto1
             var emailPoint = AutoControl.GetGlobalPoint(hWnd, 180, 190);
             var backPoint = AutoControl.GetGlobalPoint(hWnd, 450, 160);
 
-            click_to(loginPoint, 5000);
-            click_to(registerPoint, 1000);
-
-            //send username
-            click_to(usernamePoint, 0);
-            write_to("pro032132");
-
-            //send password
-            click_to(passwordPoint, 0);
-            write_to("pro032132112");
-
-            //send email
-            click_to(emailPoint, 0);
-            write_to("b4hbhweiu@gmail.com");
-
-            for(int i = 0; i < 8; i++)
+            for(int i = 0; i < lines.Length; i++)
             {
-                down_to();
+                click_to(loginPoint, 5000);
+                click_to(registerPoint, 1000);
+
+                //send username
+                click_to(usernamePoint, 0);
+                write_to(lines[i].Split('|')[0].ToString().Trim());
+                Thread.Sleep(500);
+
+                //send password
+                click_to(passwordPoint, 0);
+                write_to(lines[i].Split('|')[1].ToString().Trim());
+                Thread.Sleep(500);
+
+                //send email
+                click_to(emailPoint, 0);
+                write_to(lines[i].Split('|')[2].ToString().Trim());
+                Thread.Sleep(500);
+
+                for (int j = 0; j < 8; j++)
+                {
+                    down_to();
+                }
+
+
+                write_to("{ENTER}");
+                Thread.Sleep(1000);
+
+                click_to(backPoint, 0);
+                Thread.Sleep(3000);
             }
-
-
-            write_to("{ENTER}");
-            Thread.Sleep(1000);
-
-            click_to(backPoint, 0);
-            Thread.Sleep(3000);
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             //read file
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\20010844\Desktop\data.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\20010844\Desktop\login.txt");
             int inDexOfFile = 0;
             IntPtr[] HWND = new IntPtr[1];
             string[] screens = { "LDPlayer" };
@@ -151,11 +165,13 @@ namespace auto1
                     //SendMessage username
                     click_to(usernamePoint, 1000);
                     write_to(lines[inDexOfFile].Split('|')[0].ToString().Trim());
+                    Thread.Sleep(500);
 
 
                     //send password
                     click_to(passwordPoint, 1000);
                     write_to(lines[inDexOfFile].Split('|')[1].ToString().Trim());
+                    Thread.Sleep(500);
 
                     inDexOfFile++;
                 }
@@ -171,6 +187,36 @@ namespace auto1
 
                 //turnoff async
                 turn_off_async(HWND[0]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] play = { "play1", "play2" };
+            var subBitmap = ImageScanOpenCV.GetImage("chosenad.PNG");
+            int countX = 0;
+
+            for (int i = 0; i < play.Length; i++)
+            {
+                var handle = AutoControl.FindWindowHandle(null, play[i]);
+                var screen = (Bitmap)CaptureHelper.CaptureWindow(handle);
+
+
+                var resPoint1 = ImageScanOpenCV.FindOutPoint(screen, subBitmap);
+                Point point = new Point();
+                if (resPoint1 != null)
+                {
+                    point.X = resPoint1.Value.X + countX;
+                    point.Y = resPoint1.Value.Y;
+                    click_to(point, 400);
+
+                }
+                else
+                {
+                    MessageBox.Show("Khong tim thay");
+                }
+                countX += 490;
+                Thread.Sleep(2000);
             }
         }
     }
