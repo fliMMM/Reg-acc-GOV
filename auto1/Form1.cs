bfffb2c15b7094ec5e6a32747d30beeb5550a087
bui huy bach <bachkame@gmail.com>
2022-09-25 01:56:07 +0700

    chuyen sang bluestack

--- auto1/Form1.cs
+++ auto1/Form1.cs
@@ -37,75 +37,63 @@ namespace auto1
 
         void down_to()
         {
-            Thread.Sleep(500);
+            Thread.Sleep(300);
             AutoControl.SendKeyFocus(KeyCode.DOWN);
-        }
-
-        void turn_on_async(IntPtr HWND)
-        {
-            var _threeDotPoint = AutoControl.GetGlobalPoint(HWND, 450, 55);
-            var _displayAsyncPoint = AutoControl.GetGlobalPoint(HWND, 350, 70);
-            var _selectAllPoint = AutoControl.GetGlobalPoint(HWND, 40, 430);
-            var _startPoint = AutoControl.GetGlobalPoint(HWND, 50, 80);
-            var _loginNowPoint = AutoControl.GetGlobalPoint(HWND, 180, 210);
-
-            click_to(_threeDotPoint, 800);
-            click_to(_displayAsyncPoint, 800);
-            //click_to(_selectAllPoint, 800);
-            click_to(_startPoint, 800);
-            //click_to(_loginNowPoint, 800);
         }
 
         void play_record(IntPtr HWND, int timePerGame)
         {
-            var _threeDotPoint = AutoControl.GetGlobalPoint(HWND, 450, 55);
-            var displayRecordPoint = AutoControl.GetGlobalPoint(HWND, 300, 50);
-            var playRecordPoint = AutoControl.GetGlobalPoint(HWND, 450, 180);
-            var exitRecordPoint = AutoControl.GetGlobalPoint(HWND, 515, 85);
-            var _stopPoint = AutoControl.GetGlobalPoint(HWND, 330, 20);
+            AutoControl.BringToFront(HWND);
+            var _threeDotPoint = AutoControl.GetGlobalPoint(HWND, 458, 113);
+            var displayRecordPoint = AutoControl.GetGlobalPoint(HWND, 335, 180);
+            var playRecordPoint = AutoControl.GetGlobalPoint(HWND, 560, 175);
+            var exitRecordPoint = AutoControl.GetGlobalPoint(HWND, 625, 25);
+            var _stopPoint = AutoControl.GetGlobalPoint(HWND, 200, 15);
 
             click_to(_threeDotPoint, 800);
             click_to(displayRecordPoint, 800);
             click_to(playRecordPoint, 800);
-            click_to(exitRecordPoint, 800);
 
             //Thread.Sleep(timePerGame);
-            //click_to(_stopPoint, 700);
+            click_to(_stopPoint, 800);
+            click_to(exitRecordPoint, 800);
         }
 
-        void turn_off_async(IntPtr HWND)
+        void toggle_async(IntPtr HWND)
         {
-            var _stopPoint = AutoControl.GetGlobalPoint(HWND, 330, 20);
-            var _exitPoint = AutoControl.GetGlobalPoint(HWND, 515, 40);
-            click_to(_stopPoint, 500);
-            click_to(_exitPoint, 500);
+            var togglePoint = AutoControl.GetGlobalPoint(HWND, 257, 15);
+            //var _exitPoint = AutoControl.GetGlobalPoint(HWND, 515, 40);
+            click_to(togglePoint, 500);
+            //click_to(_exitPoint, 500);
         }
-        private void button1_Click(object sender, EventArgs e)
+
+        void reg_acc(string[] lines)
         {
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\20010844\Desktop\register.txt");
-            if(lines.Length == 0)
+            if (lines.Length == 0)
             {
                 MessageBox.Show("File khong co j ca");
                 return;
             }
 
             var hWnd = AutoControl.FindWindowHandle(null, "play1");
+            AutoControl.BringToFront(hWnd);
 
             //var openGamePoint = AutoControl.GetGlobalPoint(hWnd,225, 100);
-            var loginPoint = AutoControl.GetGlobalPoint(hWnd, 180, 230);
-            var registerPoint = AutoControl.GetGlobalPoint(hWnd, 180, 250);
-            var usernamePoint = AutoControl.GetGlobalPoint(hWnd, 180, 130);
-            var passwordPoint = AutoControl.GetGlobalPoint(hWnd, 180, 170);
-            var emailPoint = AutoControl.GetGlobalPoint(hWnd, 180, 190);
-            var backPoint = AutoControl.GetGlobalPoint(hWnd, 450, 160);
+            var loginPoint = AutoControl.GetGlobalPoint(hWnd, 160, 237);
+            var registerPoint = AutoControl.GetGlobalPoint(hWnd, 216, 208);
+            var usernamePoint = AutoControl.GetGlobalPoint(hWnd, 153, 114);
+            var passwordPoint = AutoControl.GetGlobalPoint(hWnd, 151, 139);
+            var emailPoint = AutoControl.GetGlobalPoint(hWnd, 147, 163);
+            var backPoint = AutoControl.GetGlobalPoint(hWnd, 455, 210);
+            var registerNowPoint = AutoControl.GetGlobalPoint(hWnd, 222, 270);
 
-            for(int i = 0; i < lines.Length; i++)
+            for (int i = 0; i < lines.Length; i++)
             {
                 click_to(loginPoint, 4000);
                 click_to(registerPoint, 1000);
 
                 //send username
-                click_to(usernamePoint, 0);
+                click_to(usernamePoint, 500);
                 write_to(lines[i].Split('|')[0].ToString().Trim());
                 Thread.Sleep(500);
 
@@ -119,19 +107,48 @@ namespace auto1
                 write_to(lines[i].Split('|')[2].ToString().Trim());
                 Thread.Sleep(500);
 
-                for (int j = 0; j < 8; j++)
-                {
-                    down_to();
-                }
+                
+                down_to();
+                down_to();
+                down_to();
+                down_to();
+                down_to();
 
 
-                write_to("{ENTER}");
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
+                click_to(registerNowPoint, 1000);
 
                 click_to(backPoint, 0);
-                Thread.Sleep(3000);
+                Thread.Sleep(2000);
             }
-            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines;
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\work\genAcc";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            lines = File.ReadAllLines(theDialog.FileName);
+                            reg_acc(lines);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
         }
 
         void pick_ad(IntPtr[] HWND)
@@ -142,18 +159,17 @@ namespace auto1
 
             for (int i = 0; i < HWND.Length; i++)
             {
-
+                
                 var screen = (Bitmap)CaptureHelper.CaptureWindow(HWND[i]);
 
-                screen = CaptureHelper.CropImage(screen, new System.Drawing.Rectangle(40, 83, 58, 140));
+                screen = CaptureHelper.CropImage(screen, new System.Drawing.Rectangle(24, 53, 135, 197));
                 screen.Save("linhtinh.PNG");
-
                 var resPoint1 = ImageScanOpenCV.FindOutPoint(screen, subBitmap);
                 Point point = new Point();
                 if (resPoint1 != null)
                 {
-                    point.X = resPoint1.Value.X + countX + 40;
-                    point.Y = resPoint1.Value.Y + 83;
+                    point.X = resPoint1.Value.X + countX + 30 ;
+                    point.Y = resPoint1.Value.Y  + countY + 60;
                     click_to(point, 400);
 
                 }
@@ -161,9 +177,7 @@ namespace auto1
                 if (i % 4 == 3) 
                 {
                     countX = 0;
-                    countY += 287;
                 }
-                
 
                 Thread.Sleep(500);
             }
@@ -172,8 +186,9 @@ namespace auto1
         void play_game(string[] lines)
         {
             int inDexOfFile = 0;
-            IntPtr[] HWND = new IntPtr[2];
+            
             int number_of_screen = (int)numericUpDown1.Value;
+            IntPtr[] HWND = new IntPtr[number_of_screen];
             if (number_of_screen == 0)
             {
                 MessageBox.Show("Chua nhap so luong man hinh!!");
@@ -184,7 +199,6 @@ namespace auto1
             for (int k = 0; k < number_of_screen; k++)
             {
                 screens[k] = "play" + (k + 1).ToString();
-                MessageBox.Show(screens[k]);
             }
 
             IntPtr _hWnd = IntPtr.Zero;
@@ -195,23 +209,25 @@ namespace auto1
             }
 
             //turnOn async
-            Thread.Sleep(1000);
-            turn_on_async(HWND[0]);
-            var loginPoint = AutoControl.GetGlobalPoint(HWND[0], 180, 230);
-            click_to(loginPoint, 6000);
+            toggle_async(HWND[0]);
+
+
+            var loginPoint = AutoControl.GetGlobalPoint(HWND[0], 160, 237);
+            //click_to(loginPoint, 6000);
 
             //turnoff async
-            turn_off_async(HWND[0]);
+            toggle_async(HWND[0]);
 
 
-            for (int j = 0; j < lines.Length/number_of_screen; j++)
+            for (int j = 0; j < lines.Length / number_of_screen; j++)
             {
 
-                for (int i = 0; i < HWND.Length; i++)
+                for (int i = 0; i < screens.Length; i++)
                 {
                     if (inDexOfFile == lines.Length) continue;
-                    var usernamePoint = AutoControl.GetGlobalPoint(HWND[i], 180, 130);
-                    var passwordPoint = AutoControl.GetGlobalPoint(HWND[i], 180, 160);
+                    AutoControl.BringToFront(HWND[i]);
+                    var usernamePoint = AutoControl.GetGlobalPoint(HWND[i], 163, 114);
+                    var passwordPoint = AutoControl.GetGlobalPoint(HWND[i], 154, 140);
 
                     //click_to(loginPoint, 5000);
 
@@ -231,28 +247,28 @@ namespace auto1
 
                 //turnOn async
                 Thread.Sleep(1000);
-                turn_on_async(HWND[0]);
+                toggle_async(HWND[0]);
 
                 Thread.Sleep(800);
-                var _loginNowPoint = AutoControl.GetGlobalPoint(HWND[0], 180, 210);
+                var _loginNowPoint = AutoControl.GetGlobalPoint(HWND[0], 220, 173);
                 click_to(_loginNowPoint, 800);
 
                 //turnoff async
-                turn_off_async(HWND[0]);
+                toggle_async(HWND[0]);
 
                 Thread.Sleep(13000);
                 pick_ad(HWND);
 
                 //turnOn async
                 Thread.Sleep(1000);
-                turn_on_async(HWND[0]);
+                toggle_async(HWND[0]);
 
                 //Play record
                 Thread.Sleep(1000);
-                play_record(HWND[0], 10000);
+                play_record(HWND[0], 900000);
 
-                //    //turnoff async
-                //    turn_off_async(HWND[0]);
+                //turnoff async
+                toggle_async(HWND[0]);
             }
         }
 
@@ -263,7 +279,7 @@ namespace auto1
             OpenFileDialog theDialog = new OpenFileDialog();
             theDialog.Title = "Open Text File";
             theDialog.Filter = "TXT files|*.txt";
-            theDialog.InitialDirectory = @"C:\";
+            theDialog.InitialDirectory = @"C:\work\genAcc";
             if (theDialog.ShowDialog() == DialogResult.OK)
             {
                 try
